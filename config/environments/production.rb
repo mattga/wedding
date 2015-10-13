89d6e5dd89c9fd34efcd29a9a675ee523aec1743
Matthew Gardner <mgardner091@gmail.com>
2015-10-13 14:41:23 -0500

    remove pg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.frameworks -= [ :active_record ]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -62,7 +63,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
